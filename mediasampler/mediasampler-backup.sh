2f3f66287a095911463a6be7d23da234efaa746b
Alex Langberg <mail@alexlangberg.dk>
2015-10-02 01:03:50 +0200

    Change backup solution.

--- mediasampler/mediasampler-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Use this script on another machine to connect
-# to the nginx webserver and download all files.case.
-# On debian, place in eg. /etc/cron.hourly with
-# sudo cp mediasampler-backup.sh /etc/cron.hourly
-
-wget -m -N -R "*.html" -P /path/to/folder http://localhost:8080
